51b2fa405c196170eb050280c7fb17a26d4832d6
RestartB <79792966+RestartB@users.noreply.github.com>
2025-06-14 19:42:07 +0100

    feat: Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="w-full h-full absolute top-0 left-0 -z-10 blur-xl opacity-50 bg-center bg-cover box-border" style="background-image: url('https://picsum.photos/1920/1080');"></div>
+
+<div class="min-h-screen w-full overflow-hidden">
+	{@render children()}
+</div>
