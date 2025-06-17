05526f35aa16f4217cdaaa813b87ebd95d493f96
RestartB <79792966+RestartB@users.noreply.github.com>
2025-06-17 19:07:15 +0100

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,7 +14,7 @@ export const auth = betterAuth({
 	}),
 	secret: BETTER_AUTH_SECRET,
 	baseURL: BETTER_AUTH_URL,
-	trustedOrigins: [BETTER_AUTH_URL, "https://words.restartb.xyz"],
+	trustedOrigins: [BETTER_AUTH_URL, "192.168.1.7"],
 	socialProviders: {
 		discord: {
 			clientId: DISCORD_CLIENT_ID,
