63abfb7a05b8b2c7b2899b0f1e50bbe49f19d99e
RestartB <79792966+RestartB@users.noreply.github.com>
2025-06-17 01:10:10 +0100

    fix: possible auth fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,5 +2,6 @@ import { createAuthClient } from 'better-auth/svelte';
 import { PUBLIC_BETTER_AUTH_URL } from '$env/static/public';
 
 export const authClient = createAuthClient({
-	baseURL: PUBLIC_BETTER_AUTH_URL
+	baseURL: PUBLIC_BETTER_AUTH_URL,
+	trustedOrigins: [PUBLIC_BETTER_AUTH_URL],
 });
